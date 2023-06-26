c895daa34f0938693adad0ea15ec1453e827fd66
Rozemberg Darius Andrei <135977849+xdaruis@users.noreply.github.com>
2023-06-26 12:32:02 +0200

    UI

--- script.js
+++ script.js
@@ -7,7 +7,20 @@ const dict = {
     "btn btn-primary": "btn btn-warning"     //blue
 };
 
+function activateAboveButton(element) {
+    let aboveId = element.id;
+    // ++aboveId[1];
+    // alert(aboveId);
+    // setCharAt(aboveId, 1, ++aboveId[1]);
+    // alert(aboveId[0] + ++aboveId[1]);
+    document.getElementById(aboveId[0] + (++aboveId[1])).disabled = false;
+    document.getElementById(aboveId[0] + (++aboveId[1])).innerHTML = "X";
+}
+
 function colorButton(element) {
+    // document.getElementById()
+    element.innerHTML = "&nbsp;&nbsp";
+    activateAboveButton(element);
     element.className = player;
     document.getElementById("playerMove").innerHTML = dict["color" + player] + " Move";
     player = dict[player];
