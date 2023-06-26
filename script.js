eac26bc71c124c7dc24a48c3bbed9131e4df218f
Rozemberg Darius Andrei <135977849+xdaruis@users.noreply.github.com>
2023-06-26 13:13:10 +0200

    update

--- script.js
+++ script.js
@@ -1,6 +1,93 @@
 let player = "btn btn-warning";
 
+const winnerCombos = [
+    ["a0", "b0", "c0", "d0"],
+    ["a0", "b1", "c2", "d3"],
+    ["a0", "a1", "a2", "a3"],
+    ["a1", "b1", "c1", "d1"],
+    ["a1", "b2", "c3", "d4"],
+    ["a1", "a2", "a3", "a4"],
+    ["a2", "b2", "c2", "d2"],
+    ["a2", "b3", "c4", "d5"],
+    ["a2", "a3", "a4", "a5"],
+    ["a3", "b3", "c3", "d3"],
+    ["a4", "b4", "c4", "d4"],
+    ["a5", "b5", "c5", "d5"],
+
+    ["b0", "c0", "d0", "e0"],
+    ["b0", "c1", "d2", "e3"],
+    ["b0", "b1", "b2", "b3"],
+    ["b1", "c1", "d1", "e1"],
+    ["b1", "c2", "d3", "e4"],
+    ["b1", "b2", "b3", "b4"],
+    ["b2", "c2", "d2", "e2"],
+    ["b2", "c3", "d4", "e5"],
+    ["b2", "b3", "b4", "b5"],
+    ["b3", "c3", "d3", "e3"],
+    ["b4", "c4", "d4", "e4"],
+    ["b5", "c5", "d5", "e5"],
+
+    ["c0", "d0", "e0", "f0"],
+    ["c0", "d1", "e2", "f3"],
+    ["c0", "c1", "c2", "c3"],
+    ["c1", "d1", "e1", "f1"],
+    ["c1", "d2", "e3", "f4"],
+    ["c1", "c2", "c3", "c4"],
+    ["c2", "d2", "e2", "f2"],
+    ["c2", "d3", "e4", "f5"],
+    ["c2", "c3", "c4", "c5"],
+    ["c3", "d3", "e3", "f3"],
+    ["c4", "d4", "e4", "f4"],
+    ["c5", "d5", "e5", "f5"],
+
+    ["d0", "e0", "f0", "g0"],
+    ["d0", "e1", "f2", "g3"],
+    ["d0", "d1", "d2", "d3"],
+    ["d1", "e1", "f1", "g1"],
+    ["d1", "e2", "f3", "g4"],
+    ["d1", "d2", "d3", "d4"],
+    ["d2", "e2", "f2", "g2"],
+    ["d2", "e3", "f4", "g5"],
+    ["d2", "d3", "d4", "d5"],
+    ["d3", "e3", "f3", "g3"],
+    ["d4", "e4", "f4", "g4"],
+    ["d5", "e5", "f5", "g5"],
+
+    ["d0", "c1", "b2", "a3"],
+    ["d1", "c2", "b3", "a4"],
+    ["d2", "c3", "b4", "a5"],
+
+    ["e0", "d1", "c2", "b3"],
+    ["e1", "d2", "c3", "b4"],
+    ["e2", "d3", "c4", "b5"],
+    
+    ["e0", "e1", "e2", "e3"],
+    ["e1", "e2", "e3", "e4"],
+    ["e2", "e3", "e4", "e5"],
+    
+    ["f0", "e1", "d2", "c3"],
+    ["f1", "e2", "d3", "c4"],
+    ["f2", "e3", "d4", "c5"],
+    
+    ["f0", "f1", "f2", "f3"],
+    ["f1", "f2", "f3", "f4"],
+    ["f2", "f3", "f4", "f5"],
+
+    ["g0", "f1", "e2", "d3"],
+    ["g1", "f2", "e3", "d4"],
+    ["g2", "f3", "e4", "d5"],
+    
+    ["g0", "g1", "g2", "g3"],
+    ["g1", "g2", "g3", "g4"],
+    ["g2", "g3", "g4", "g5"],
+    
+];
+
+const movesYellow = [], movesBlue = [];
+
 const dict = {
+    "movesbtn btn-warning": movesYellow,
+    "movesbtn btn-primary": movesBlue,
     "colorbtn btn-warning": "Blue",
     "colorbtn btn-primary": "Yellow",
     "btn btn-warning": "btn btn-primary",
@@ -17,6 +104,9 @@ function colorButton(element) {
     element.innerHTML = "&nbsp;&nbsp";
     element.className = player;
     document.getElementById("playerMove").innerHTML = dict["color" + player] + " Move";
+    dict["moves" + player].push(element.id);
+    // alert("HI");
+    // alert(dict["moves" + player]);
     player = dict[player];
     activateAboveButton(element);
 }
