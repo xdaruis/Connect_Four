f7474d7babac3742ae7d89a2fef0997be17f8bb2
Rozemberg Darius Andrei <135977849+xdaruis@users.noreply.github.com>
2023-06-26 12:35:01 +0200

    UI

--- script.js
+++ script.js
@@ -3,25 +3,20 @@ let player = "btn btn-warning";
 const dict = {
     "colorbtn btn-warning": "Blue",
     "colorbtn btn-primary": "Yellow",
-    "btn btn-warning": "btn btn-primary", //yellow 
-    "btn btn-primary": "btn btn-warning"     //blue
+    "btn btn-warning": "btn btn-primary",
+    "btn btn-primary": "btn btn-warning"
 };
 
 function activateAboveButton(element) {
     let aboveId = element.id;
-    // ++aboveId[1];
-    // alert(aboveId);
-    // setCharAt(aboveId, 1, ++aboveId[1]);
-    // alert(aboveId[0] + ++aboveId[1]);
-    document.getElementById(aboveId[0] + (++aboveId[1])).disabled = false;
-    document.getElementById(aboveId[0] + (++aboveId[1])).innerHTML = "X";
+    document.getElementById(aboveId[0] + ++aboveId[1]).disabled = false;
+    document.getElementById(aboveId[0] + ++aboveId[1]).innerHTML = "X";
 }
 
 function colorButton(element) {
-    // document.getElementById()
     element.innerHTML = "&nbsp;&nbsp";
-    activateAboveButton(element);
     element.className = player;
     document.getElementById("playerMove").innerHTML = dict["color" + player] + " Move";
     player = dict[player];
+    activateAboveButton(element);
 }
